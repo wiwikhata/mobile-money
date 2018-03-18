7b87f857b2243b9f295cd164346da9f485b67414
Samuel Dervis <samueldervis@gmail.com>
2018-03-18 12:27:36 +0300

    Fire events

--- src/Repositories/Mpesa.php
+++ src/Repositories/Mpesa.php
@@ -7,6 +7,7 @@ use DervisGroup\Pesa\Database\Entities\MpesaBulkPaymentResponse;
 use DervisGroup\Pesa\Database\Entities\MpesaC2bCallback;
 use DervisGroup\Pesa\Database\Entities\MpesaStkCallback;
 use DervisGroup\Pesa\Events\B2cPaymentFailedEvent;
+use DervisGroup\Pesa\Events\B2cPaymentSuccessEvent;
 use DervisGroup\Pesa\Events\C2bConfirmationEvent;
 use Gahlawat\Slack\Facade\Slack;
 
@@ -69,25 +70,32 @@ class Mpesa
         return $callback;
     }
 
+    /**
+     * @return MpesaBulkPaymentResponse
+     */
     private function handleB2cResult()
     {
         $data = json_decode(request('Result'), true);
         $common = [
             'ResultType', 'ResultCode', 'ResultDesc', 'OriginatorConversationID', 'ConversationID', 'TransactionID'
         ];
-        if ($data['ResultCode'] != 0) {
+        if ($data['ResultCode'] !== 0) {
             $response = MpesaBulkPaymentResponse::create($data, array_only($data, $common));
             event(new B2cPaymentFailedEvent($response->request, $data));
+            return $response;
         }
-        return MpesaBulkPaymentResponse::create($data, array_except($data, ['ResultParameters', 'ReferenceData']));
+        $response = MpesaBulkPaymentResponse::create($data, array_except($data, ['ResultParameters', 'ReferenceData']));
+        event(new B2cPaymentSuccessEvent($response->request, $data));
+        return $response;
     }
 
     /**
-     * @param null $initiator
+     * @param string|null $initiator
+     * @return MpesaBulkPaymentResponse|void
      */
     public function handleResult($initiator = null)
     {
-        if ($initiator == 'b2c') {
+        if ($initiator === 'b2c') {
             return $this->handleB2cResult();
         }
         return;
